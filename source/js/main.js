cb029b7a8e6ed977c6867482c07eeb6cb5c495e5
valkiboy <val_kik@mail.ru>
2022-03-20 11:12:32 +0300

    Добавил немного js и альты для фото

--- source/js/main.js
+++ source/js/main.js
@@ -12,3 +12,97 @@ navToggle.addEventListener('click', function () {
     pageHeader.classList.remove('page-header--open');
   }
 });
+
+// const pageHeaderAdvantages = document.querySelector('.page-header__advantages');
+
+
+const trigger = document.querySelector('.js-trigger');
+
+const moveTo = new MoveTo({
+  tolerance: 0,
+  duration: 800,
+  easing: 'easeOutQuart',
+  container: window,
+});
+
+moveTo.registerTrigger(trigger);
+
+const myName = document.getElementById('name');
+const myMail = document.getElementById('email');
+const myPhone = document.getElementById('telephone');
+const rename = /^[a-zA-Zа-яА-Я']+[a-zA-Zа-яА-Я']?$/u;
+const remail = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+const rephone = /([0-9]+)$/;
+const bookingForm = document.querySelector('.booking__form');
+
+
+const checkNameValidity = () => {
+
+  const textName = myName.value;
+
+  if (textName === '') {
+    myName.setCustomValidity('поле обязательное');
+  } else if (!rename.test(textName)) {
+    myName.setCustomValidity('имя может содержать только буквы');
+  } else {
+    myName.setCustomValidity('');
+  }
+  myName.reportValidity();
+};
+
+const checkPhoneValidity = () => {
+  const textPhone = myPhone.value;
+
+  if (textPhone === '') {
+    myPhone.setCustomValidity('поле обязательное');
+  } else if (!rephone.test(textPhone)) {
+    myPhone.setCustomValidity('номер телефона должен содержать только цифры');
+  } else {
+    myPhone.setCustomValidity('');
+  }
+  myPhone.reportValidity();
+};
+
+const checkMailValidity = () => {
+  const textMail = myMail.value;
+
+  if (textMail === '') {
+    myMail.setCustomValidity('поле обязательное');
+  } else if (!remail.test(textMail)) {
+    myMail.setCustomValidity('адрес почты должен выглядеть так "имя_пользователя@имя_сервера"');
+  } else {
+    myMail.setCustomValidity('');
+  }
+  myMail.reportValidity();
+};
+
+
+myName.addEventListener('input', checkNameValidity);
+myPhone.addEventListener('input', checkPhoneValidity);
+myMail.addEventListener('input', checkMailValidity);
+
+
+const handleFormSubmit = (evt) => {
+  evt.preventDefault();
+  const formData = new FormData(evt.target);
+  fetch(
+      'https://echo.htmlacademy.ru/',
+      {
+        method: 'POST',
+        body: formData,
+      }
+  );
+  evt.target.reset();
+};
+
+bookingForm.addEventListener('submit', handleFormSubmit);
+
+function persist(event) {
+  let thisArg = event.path[0];
+  localStorage.setItem(thisArg.id, thisArg.value);
+}
+
+document.querySelectorAll('input').forEach((inputEl) => {
+  inputEl.value = localStorage.getItem(inputEl.id);
+  inputEl.addEventListener('change', persist);
+});
