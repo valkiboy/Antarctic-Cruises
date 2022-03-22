e9dbf880fa140bc001502b268fea452b17a03bf8
valkiboy <val_kik@mail.ru>
2022-03-22 23:01:23 +0300

    внес исправления

--- source/js/main.js
+++ source/js/main.js
@@ -1,17 +1,21 @@
 const pageHeader = document.querySelector('.page-header');
 const navToggle = document.querySelector('.main-nav__toggle');
 
-pageHeader.classList.add('page-header--closed');
+if (typeof (pageHeader && navToggle) !== 'undefined' && pageHeader && navToggle !== null) {
 
-navToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--open');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--open');
-  }
-});
+  pageHeader.classList.add('page-header--closed');
+
+  navToggle.addEventListener('click', function () {
+    if (pageHeader.classList.contains('page-header--closed')) {
+      pageHeader.classList.remove('page-header--closed');
+      pageHeader.classList.add('page-header--open');
+    } else {
+      pageHeader.classList.add('page-header--closed');
+      pageHeader.classList.remove('page-header--open');
+    }
+  });
+}
+
 
 const myName = document.getElementById('name');
 const myMail = document.getElementById('email');
@@ -21,67 +25,66 @@ const remail = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
 const rephone = /([0-9]+)$/;
 const bookingForm = document.querySelector('.booking__form');
 
+if (typeof (myName) !== 'undefined' && myName !== null) {
+  const checkNameValidity = () => {
+    const textName = myName.value;
+    if (textName === '') {
+      myName.setCustomValidity('поле обязательное');
+    } else if (!rename.test(textName)) {
+      myName.setCustomValidity('имя может содержать только буквы');
+    } else {
+      myName.setCustomValidity('');
+    }
+    myName.reportValidity();
+  };
+  myName.addEventListener('input', checkNameValidity);
+}
 
-const checkNameValidity = () => {
+if (typeof (myPhone) !== 'undefined' && myPhone !== null) {
+  const checkPhoneValidity = () => {
+    const textPhone = myPhone.value;
+    if (textPhone === '') {
+      myPhone.setCustomValidity('поле обязательное');
+    } else if (!rephone.test(textPhone)) {
+      myPhone.setCustomValidity('номер телефона должен содержать только цифры');
+    } else {
+      myPhone.setCustomValidity('');
+    }
+    myPhone.reportValidity();
+  };
+  myPhone.addEventListener('input', checkPhoneValidity);
+}
 
-  const textName = myName.value;
+if (typeof (myMail) !== 'undefined' && myMail !== null) {
+  const checkMailValidity = () => {
+    const textMail = myMail.value;
+    if (textMail === '') {
+      myMail.setCustomValidity('поле обязательное');
+    } else if (!remail.test(textMail)) {
+      myMail.setCustomValidity('адрес почты должен выглядеть так "имя_пользователя@имя_сервера"');
+    } else {
+      myMail.setCustomValidity('');
+    }
+    myMail.reportValidity();
+  };
+  myMail.addEventListener('input', checkMailValidity);
+}
 
-  if (textName === '') {
-    myName.setCustomValidity('поле обязательное');
-  } else if (!rename.test(textName)) {
-    myName.setCustomValidity('имя может содержать только буквы');
-  } else {
-    myName.setCustomValidity('');
-  }
-  myName.reportValidity();
-};
-
-const checkPhoneValidity = () => {
-  const textPhone = myPhone.value;
-
-  if (textPhone === '') {
-    myPhone.setCustomValidity('поле обязательное');
-  } else if (!rephone.test(textPhone)) {
-    myPhone.setCustomValidity('номер телефона должен содержать только цифры');
-  } else {
-    myPhone.setCustomValidity('');
-  }
-  myPhone.reportValidity();
-};
-
-const checkMailValidity = () => {
-  const textMail = myMail.value;
-
-  if (textMail === '') {
-    myMail.setCustomValidity('поле обязательное');
-  } else if (!remail.test(textMail)) {
-    myMail.setCustomValidity('адрес почты должен выглядеть так "имя_пользователя@имя_сервера"');
-  } else {
-    myMail.setCustomValidity('');
-  }
-  myMail.reportValidity();
-};
-
-
-myName.addEventListener('input', checkNameValidity);
-myPhone.addEventListener('input', checkPhoneValidity);
-myMail.addEventListener('input', checkMailValidity);
-
-
-const handleFormSubmit = (evt) => {
-  evt.preventDefault();
-  const formData = new FormData(evt.target);
-  fetch(
-      'https://echo.htmlacademy.ru/',
-      {
-        method: 'POST',
-        body: formData,
-      }
-  );
-  evt.target.reset();
-};
-
-bookingForm.addEventListener('submit', handleFormSubmit);
+if (typeof (bookingForm) !== 'undefined' && bookingForm !== null) {
+  const handleFormSubmit = (evt) => {
+    evt.preventDefault();
+    const formData = new FormData(evt.target);
+    fetch(
+        'https://echo.htmlacademy.ru/',
+        {
+          method: 'POST',
+          body: formData,
+        }
+    );
+    evt.target.reset();
+  };
+  bookingForm.addEventListener('submit', handleFormSubmit);
+}
 
 function persist(event) {
   let thisArg = event.path[0];
